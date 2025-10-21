eb3eebff0d6ac91ed5f5091f20b6fcecc2ff6068
PJ Fanning <pjfanning@users.noreply.github.com>
2025-10-21 14:17:28 +0100

    support java use in Jackson libs

--- pekko-http-jackson/src/main/java/com/github/pjfanning/pekkohttpjackson/Jackson.java
+++ pekko-http-jackson/src/main/java/com/github/pjfanning/pekkohttpjackson/Jackson.java
@@ -13,18 +13,45 @@
 
 package com.github.pjfanning.pekkohttpjackson;
 
+import org.apache.pekko.http.javadsl.model.HttpEntity;
 import org.apache.pekko.http.javadsl.model.MediaTypes;
 import org.apache.pekko.http.javadsl.model.RequestEntity;
 import org.apache.pekko.http.javadsl.marshalling.Marshaller;
+import org.apache.pekko.http.javadsl.unmarshalling.Unmarshaller;
+import org.apache.pekko.http.scaladsl.model.ErrorInfo;
+import org.apache.pekko.http.scaladsl.model.ExceptionWithErrorInfo;
+import org.apache.pekko.util.ByteString;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-final class Jackson {
+import java.io.IOException;
 
-    static <T> Marshaller<T, RequestEntity> marshaller(ObjectMapper mapper) {
+public class Jackson {
+
+    public static class JacksonUnmarshallingException extends ExceptionWithErrorInfo {
+        public JacksonUnmarshallingException(Class<?> expectedType, IOException cause) {
+            super(
+                    new ErrorInfo(
+                            "Cannot unmarshal JSON as " + expectedType.getSimpleName(), cause.getMessage()),
+                    cause);
+        }
+    }
+
+    public static <T> Marshaller<T, RequestEntity> marshaller(ObjectMapper mapper) {
         return Marshaller.wrapEntity(
                 u -> toJSON(mapper, u), Marshaller.stringToEntity(), MediaTypes.APPLICATION_JSON);
+    }
+
+    public static <T> Unmarshaller<HttpEntity, T> unmarshaller(
+            ObjectMapper mapper, Class<T> expectedType) {
+        return Unmarshaller.forMediaType(MediaTypes.APPLICATION_JSON, Unmarshaller.entityToString())
+                .thenApply(s -> fromJSON(mapper, s, expectedType));
+    }
+
+    public static <T> Unmarshaller<ByteString, T> byteStringUnmarshaller(
+            ObjectMapper mapper, Class<T> expectedType) {
+        return Unmarshaller.sync(s -> fromJSON(mapper, s.utf8String(), expectedType));
     }
 
     private static String toJSON(ObjectMapper mapper, Object object) {
@@ -32,6 +59,14 @@ final class Jackson {
             return mapper.writeValueAsString(object);
         } catch (JacksonException e) {
             throw new IllegalArgumentException("Cannot marshal to JSON: " + object, e);
+        }
+    }
+
+    private static <T> T fromJSON(ObjectMapper mapper, String json, Class<T> expectedType) {
+        try {
+            return mapper.readerFor(expectedType).readValue(json);
+        } catch (IOException e) {
+            throw new JacksonUnmarshallingException(expectedType, e);
         }
     }
 
